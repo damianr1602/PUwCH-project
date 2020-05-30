778f5a9286984859e76872a87913cb3be354ffa0
damian <damianr1602@gmail.com>
2020-05-30 21:43:46 +0200

    swagger def

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/damianr1602/chmuryrest
+
+go 1.13
+
+require (
+	github.com/alexflint/go-arg v1.3.0
+	github.com/cirocosta/hello-swagger v0.0.0-20171205230352-ad98fd475b6d
+	github.com/go-openapi/loads v0.19.5
+	github.com/go-openapi/runtime v0.19.15
+	github.com/go-openapi/swag v0.19.9
+	github.com/jessevdk/go-flags v1.4.0 // indirect
+	github.com/tylerb/graceful v1.2.15 // indirect
+)
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/alexflint/go-arg"
+	"github.com/cirocosta/hello-swagger/swagger/models"
+	"github.com/cirocosta/hello-swagger/swagger/restapi"
+	"github.com/cirocosta/hello-swagger/swagger/restapi/operations"
+	"github.com/go-openapi/swag"
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
+)
+
+
+type cliArgs struct {
+	Port int `arg:"-p,help:port to listen to"`
+}
+
+var (
+	// args is a reference to an instantiation of
+	// the configuration that the CLI expects but
+	// with some values set.
+	// By setting some values in advance we provide
+	// default values that the user might provide
+	// or not.
+	args = &cliArgs{
+		Port: 8080,
+	}
+)
+
+// main parses the arguments from the CLI as specified
+// by our configuration described in `cliArgs` and then
+// populates the `args` reference we defined in the `vars`
+// section above.
+func main () {
+	arg.MustParse(args)
+
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	api := operations.NewHelloAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+	defer server.Shutdown()
+
+	server.Port = args.Port
+
+
+        // Implement the handler functionality.
+        // As all we need to do is give an implementation to the interface
+        // we can just override the `api` method giving it a method with a valid
+        // signature (we didn't need to have this implementation here, it could
+        // even come from a different package).
+	api.GetHostnameHandler = operations.GetHostnameHandlerFunc(
+		func(params operations.GetHostnameParams) middleware.Responder {
+			response, err := os.Hostname()
+			if err != nil {
+				return operations.NewGetHostnameDefault(500).WithPayload(&models.Error{
+					Code: 500,
+					Message: swag.String("failed to retrieve hostname"),
+				})
+			}
+
+			return operations.NewGetHostnameOK().WithPayload(response)
+		})
+
+        // Start listening using having the handlers and port 
+        // already set up.
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
+
+}
